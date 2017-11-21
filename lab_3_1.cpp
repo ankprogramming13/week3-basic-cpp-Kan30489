c4a851467dc48578214ebe1c9fd10bfe7e5f98b4
Unknown <Kan_30489@hotmail.com>
2017-11-21 17:25:54 +0700

    ทำข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "Miss" << "kanjana pongbuli" << endl;
+  cout << "kan" << endl;
+  cout << "female" << endl;
+  cout << "29 march 1999" << endl;
+  cout << "watch a movie" << endl;
+  cout << "single";
   return 0;
 }
